8b56c7a5492330220232b77740a64a33e84631c3
paul <syrovatskyi93@gmail.com>
2020-11-30 23:28:51 +0200

    Add is done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,63 @@
 import React, { Component } from 'react';
+import ListItems from './components/ListItems'
 import './App.css';
 
 class App extends Component {
+
+
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [],
+      currentItem: {
+        text: '',
+        key: ''
+      }
+    }
+  }
+
+  handleInput = (e) => {
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+
+  addItem = (e) => {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if(newItem.text !== "") {
+      const newItems = [...this.state.items, newItem];
+      this.setState({
+        items: newItems,
+        currentItem: {
+          text: '',
+          key: ''
+        }
+      })
+    }
+  }
   render() {
+
+
     return (
       <div className="App">
         <header>
-          <form id="to-do-form">
+          <form id="to-do-form" onSubmit={this.addItem}>
             <input
               type="text"
               placeholder="Enter Text"
+              value={this.state.currentItem.text}
+              onChange={this.handleInput}
             />
 
             <button type="submit">Add</button>
           </form>
         </header>
+
+        <ListItems items={this.state.items}></ListItems>
       </div>
 
     )
